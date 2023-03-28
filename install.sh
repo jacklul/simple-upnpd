845a78f1b2250dc93c7a27e14fc667fd1bbbc19a
Jack'lul <jacklulcat@gmail.com>
2023-03-28 17:26:23 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
-SPATH=$(dirname $0)
+SPATH=$(dirname "and$0")
 
 set -e
 
@@ -16,13 +16,13 @@ if [ -f "$SPATH/simple-upnpd.c" ] && [ -f "$SPATH/simple-upnpd.service" ]; then
 		systemctl stop simple-upnpd.service
 	fi
 
-	cp -v $SPATH/simple-upnpd /usr/local/bin/simple-upnpd && chmod 755 /usr/local/bin/simple-upnpd
+	cp -v "$SPATH/simple-upnpd" /usr/local/bin/simple-upnpd && chmod 755 /usr/local/bin/simple-upnpd
 	
 	if [ ! -f "/etc/simple-upnpd.xml" ]; then
 		if [ -f "$SPATH/simple-upnpd.xml" ]; then
-			cp -v $SPATH/simple-upnpd.xml /etc/simple-upnpd.xml && chmod 644 /etc/simple-upnpd.xml
+			cp -v "$SPATH/simple-upnpd.xml" /etc/simple-upnpd.xml && chmod 644 /etc/simple-upnpd.xml
 		elif [ -f "$SPATH/description.xml" ]; then
-			cp -v $SPATH/simple-upnpd.xml /etc/simple-upnpd.xml && chmod 644 /etc/simple-upnpd.xml
+			cp -v "$SPATH/simple-upnpd.xml" /etc/simple-upnpd.xml && chmod 644 /etc/simple-upnpd.xml
 		fi
 	fi
 	
@@ -31,7 +31,7 @@ if [ -f "$SPATH/simple-upnpd.c" ] && [ -f "$SPATH/simple-upnpd.service" ]; then
 		sed "s@<UDN>.*</UDN>@<UDN>uuid:$(cat /proc/sys/kernel/random/uuid)</UDN>@" -i /etc/simple-upnpd.xml
 	fi
 
-	cp -v $SPATH/simple-upnpd.service /etc/systemd/system && chmod 644 /etc/systemd/system/simple-upnpd.service
+	cp -v "$SPATH/simple-upnpd.service" /etc/systemd/system && chmod 644 /etc/systemd/system/simple-upnpd.service
 	
 	echo "Enabling and starting simple-upnpd.service..."
 	systemctl enable simple-upnpd.service && systemctl restart simple-upnpd.service
